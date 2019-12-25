b96c75b8ade2b17626454c2f16fc34db672a4143
m <m@example.org>
2019-12-25 10:10:55 +0000

    Backported the mk2's hsv function + tidying

--- lib/Games/SpaceEngineers/BluePrint/Role/XMLHandler.pm
+++ lib/Games/SpaceEngineers/BluePrint/Role/XMLHandler.pm
@@ -1,87 +1,117 @@
 package Games::SpaceEngineers::BluePrint::Role::XMLHandler;
-our $VERSION = '0.02';
+our $VERSION = '0.04';
 
-##~ DIGEST : ff5b2c58f984d755d90bb5ecef41f588
+##~ DIGEST : f501a3fe8be8e3021639fdeaf470ecd1
 # ABSTRACT: Moose role for constructing SE blueprint XML using XML::LibXML with guard rails
 use Moo::Role;
 use Carp qw/confess/;
-use Convert::Colour;
+use XML::LibXML;
+use XML::LibXML::PrettyPrint;
 
 sub get_xml_component {
 	my ( $self, $p ) = @_;
 	my $eparams = {%{$p}};
-	for ( qw/x y z SubtypeName/ ) {
-		confess "Missing element [$_]" unless $eparams->{$_};
+	for ( qw/ SubtypeName/ ) {
+		confess "Missing element [$_]" unless defined( $eparams->{$_} );
 	}
 
 	# 'The Thing'
-	my $grid_def = XML::LibXML::Element->new( 'MyObjectBuilder_CubeBlock' );
-	$subtype->appendText( $p->{SubtypeName} );
+	my $this_block    = XML::LibXML::Element->new( 'MyObjectBuilder_CubeBlock' );
+	my $subtype_block = XML::LibXML::Element->new( 'SubtypeName' );
+	$subtype_block->appendText( $p->{SubtypeName} );
+	$this_block->addChild( $subtype_block );
 
 	#The actual block type, e.g. heavy armor, refinery etc
 	my $component = XML::LibXML::Element->new( 'SubtypeName' );
 
-	$grid_def->setAttribute( 'xsi:type', 'MyObjectBuilder_CubeBlock' );
-
-	POSITION: {
-		my $min = XML::LibXML::Element->new( 'Min' );
-		for ( qw/x y z/ ) {
-			$min->setAttribute( $_, $p->{$_} );
-		}
-		$grid_def->addChild( $min );
-	}
-
-	ORIENTATION: {
-
-		my $orientation = XML::LibXML::Element->new( 'BlockOrientation' );
-		$orientation->setAttribute( 'Forward', $eparams->{Forward} || 'Forward' );
-		$orientation->setAttribute( 'Up',      $eparams->{Up}      || 'Up' );
-		$grid_def->addChild( $orientation );
-	}
-
-	COLOUR: {
-		my $colour = XML::LibXML::Element->new( 'ColorMaskHSV' );
-
-		# TODO re-find how to pass a hash slice in key order
-		my ( $h, $s, $v ) = Meese::SpaceEngineers::Img::rgb_to_hsv( $p->{r}, $p->{g}, $p->{b} );
-
-		#yes really, xyz
-		$colour->setAttribute( 'x', $h );
-		$colour->setAttribute( 'y', $s );
-		$colour->setAttribute( 'z', $v );
-		$grid_def->addChild( $colour );
-	}
+	$this_block->setAttribute( 'xsi:type', 'MyObjectBuilder_CubeBlock' );
 
 	#Wild mass guessing suggests this is to do with owner attributes,e.g. who owns a turret
 	SHARE: {
 		my $share = XML::LibXML::Element->new( 'ShareMode' );
 		$share->appendText( $p->{ShareMode} || 'None' );
-		$grid_def->addChild( $share );
+		$this_block->addChild( $share );
 	}
 
 	#This could get *really* interesting
 	DEFORMATION: {
 		my $deform = XML::LibXML::Element->new( 'DeformationRatio' );
 		$deform->appendText( '0' );
-		$grid_def->addChild( $deform );
+		$this_block->addChild( $deform );
 	}
-	return $grid_def;
+	return $this_block;
+}
+
+sub set_position {
+	my ( $self, $this_block, $p ) = @_;
+
+	for ( qw/x y z / ) {
+		confess "Missing element [$_]" unless defined( $p->{$_} );
+	}
+
+	my $min = XML::LibXML::Element->new( 'Min' );
+	for ( qw/x y z/ ) {
+		$min->setAttribute( $_, $p->{$_} );
+	}
+	$this_block->addChild( $min );
+	return $this_block;
+
+}
+
+sub set_orientation {
+
+	my ( $self, $this_block, $p ) = @_;
+	$p ||= {};
+	my $orientation = XML::LibXML::Element->new( 'BlockOrientation' );
+
+	$orientation->setAttribute( 'Forward', $p->{Forward} || 'Forward' );
+	$orientation->setAttribute( 'Up',      $p->{Up}      || 'Up' );
+	$this_block->addChild( $orientation );
+	return $this_block;
+
+}
+
+sub set_colour {
+	my ( $self, $this_block, $colour_proto ) = @_;
+	my $this_colour = $colour_proto->cloneNode( 1 );
+	$this_block->addChild( $this_colour );
+}
+
+# because colour order matters and has to come after position ?!!?!
+sub get_colour_proto {
+	my ( $self, $p ) = @_;
+
+	my $colour_block = XML::LibXML::Element->new( 'ColorMaskHSV' );
+
+	# TODO re-find how to pass a hash slice in key order
+	my ( $h, $s, $v ) = $self->rgb_to_hsv( $p->{r}, $p->{g}, $p->{b} );
+
+	#yes really, xyz
+	$colour_block->setAttribute( 'x', $h );
+	$colour_block->setAttribute( 'y', $s );
+	$colour_block->setAttribute( 'z', $v );
+	return $colour_block;
+
 }
 
 sub rgb_to_hsv {
 	my ( $self, $r, $g, $b ) = @_;
 
 	# TODO cache ?
+	require Convert::Color;
 	my $color = Convert::Color->new( sprintf( 'rgb8:%s,%s,%s', $r, $g, $b ) );
 	my ( $h, $s, $v ) = $color->as_hsv->hsv() or die $!;
-	$h = $h / 360; # hue in SE is percentage of 360 apparently
+	warn "$h, $s, $v becoming";
+	$h = $h / 360;       # hue in SE is percentage of 360 apparently
+	$s = ( $s * 2 ) - 1; # and we think that saturation is on a -1 to 1 scale
+	warn "\t$h, $s, $v";
 	return ( $h, $s, $v );
 
 }
 
 sub render_xml {
 	my ( $self, $xml_obj ) = @_;
-	XML::LibXML::PrettyPrint->new( indent_string => "", new_line => '' )->pretty_print( $xml_obj );
+	XML::LibXML::PrettyPrint->new( indent_string => '', new_line => $/ )->pretty_print( $xml_obj );
 
 }
 
@@ -91,7 +121,7 @@ sub render_xml {
 
 sub render_in_template {
 	my ( $self, $p ) = @_;
-	confess "Can't do anything without a grid parameter" unless $p->{grid};
+	confess "Can't do anything without a grid_string parameter" unless $p->{grid_string};
 	my $ep = {%{$p}};
 	$ep->{name}         ||= 'Generated Blueprint';
 	$ep->{owner_name}   ||= 'Not Applicable';
@@ -102,29 +132,34 @@ sub render_in_template {
 <Definitions xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 	<ShipBlueprints>
 		<ShipBlueprint>
-			<Id>
-				<TypeId>MyObjectBuilder_ShipBlueprintDefinition</TypeId>
-				<SubtypeId>$ep->{name}</SubtypeId>
-			</Id>
+			<Id Type="MyObjectBuilder_ShipBlueprintDefinition" Subtype="$ep->{name}" />
+
 			<DisplayName>$ep->{owner_name}</DisplayName>
 			<CubeGrids>
 				<CubeGrid>
 					<EntityId>$ep->{EntityId}</EntityId>
 					<PersistentFlags>CastShadows InScene</PersistentFlags>
-					<PositionAndOrientation>
-						<Position x="14.898984909057617" y="2.6750001907348633" z="-11.799983978271484" />
-						<Forward x="1" y="-0" z="-0" />
-						<Up x="0" y="1" z="0" />
-					</PositionAndOrientation>
+          <PositionAndOrientation>
+            <Position x="0" y="0" z="0" />
+            <Forward x="-0" y="-0" z="-1" />
+            <Up x="0" y="1" z="0" />
+            <Orientation>
+              <X>0</X>
+              <Y>0</Y>
+              <Z>0</Z>
+              <W>1</W>
+            </Orientation>
+          </PositionAndOrientation>
 					<GridSizeEnum>Large</GridSizeEnum>
-					<CubeBlocks>
-						$p->{grid}
-					</CubeBlocks>
+						$p->{grid_string}
 					<DisplayName>$ep->{name}</DisplayName>
-					<DestructibleBlocks>true</DestructibleBlocks>
-					<CreatePhysics>false</CreatePhysics>
-					<EnableSmallToLargeConnections>false</EnableSmallToLargeConnections>
-				</CubeGrid>
+          <DestructibleBlocks>true</DestructibleBlocks>
+          <CreatePhysics>false</CreatePhysics>
+          <EnableSmallToLargeConnections>false</EnableSmallToLargeConnections>
+          <IsRespawnGrid>false</IsRespawnGrid>
+          <LocalCoordSys>0</LocalCoordSys>
+          <TargetingTargets />
+        </CubeGrid>
 			</CubeGrids>
 			<WorkshopId>0</WorkshopId>
 			<OwnerSteamId>$ep->{OwnerSteamId}</OwnerSteamId>
